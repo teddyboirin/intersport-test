b9c6e2ad051c792d2dddccd3ad30356f8ecedb96
Teddy <t.boirin@icloud.com>
2023-07-16 00:56:38 +0200

    finish part 3

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Menu from './components/Menu';
 import Head from './components/Head';
 import ProductSlider from './components/ProductSlider';
 import CategoriesDoor from './components/CategoriesDoor';
+import Middle from './components/Middle';
 
 function App() {
   return (
     <div className="App">
-      <header>
+      {/* <header>
         <div className="header__first_block"></div>
         <Menu />
         <Breadcrumb />
-      </header>
-       <Head />
-       <ProductSlider />
-       <CategoriesDoor />
+      </header> */}
+      <Head />
+      <ProductSlider />
+      <CategoriesDoor />
+      <Middle />
     </div>
   );
 }
